4aa42b2d57648e263e419b648441ba7f4854cf5c
Troels Munk <tm@troelsmunk.com>
2026-03-02 12:36:37 +0100

    Create Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import firebase from "svelte-adapter-firebase";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-    adapter: firebase(),
+		adapter: adapter()
 	}
 };
 
 export default config;
-  
